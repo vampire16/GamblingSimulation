96b79bc6eb3d04f4a9a30614752a648f9cc7f29b
vampire16 <siddheshsalvi525@gmai.com>
2020-02-29 18:21:01 +0530

    ADDED : Twenty days total amount won or lost

--- gamblingsimulation/Gambler.sh
+++ gamblingsimulation/Gambler.sh
@@ -11,15 +11,31 @@ BET=1
 
 #VARIABLES
 stake=$MYSTAKE
+win=0
+loose=0
 
 #CHECK WIN OR LOOSE
-while (( stake >= MINFIFTYPERCENT && stake <= MAXFIFTYPERCENT ))
+for (( index=1; index<=20; index++ ))
 do
-	randomCheck=$((RANDOM%2))
-	if (( randomCheck == 0 ))
+	stake=$MYSTAKE
+	while (( stake >= MINFIFTYPERCENT && stake <= MAXFIFTYPERCENT ))
+	do
+		randomCheck=$((RANDOM%2))
+		if (( randomCheck == 0 ))
+		then
+			stake=$(($stake+$BET))
+		else
+			stake=$(($stake-$BET))
+		fi
+	done
+	if (( stake > MAXFIFTYPERCENT ))
 	then
-		stake=$(($stake+$BET))
+		((win++))
 	else
-		stake=$(($stake-$BET))
+		((loose++))
 	fi
 done
+totalWin=$(($win*$FIFTYPERCENTSTAKE))
+totalLoose=$(($loose*$FIFTYPERCENTSTAKE))
+echo "Total win for a day : $totalWin"
+echo "Total loose for a day : $totalLoose"
