c0f9303f5b95ef2ffb74b045ec5097a8dab3c417
vampire16 <siddheshsalvi525@gmai.com>
2020-02-29 17:15:39 +0530

    ADDED : If won or lost 50% of the stake,would resign for the day

--- gamblingsimulation/Gambler.sh
+++ gamblingsimulation/Gambler.sh
@@ -4,16 +4,22 @@ echo "Welcome"
 
 #CONSTANT VARIABLES
 MYSTAKE=100
+FIFTYPERCENTSTAKE=$(($MYSTAKE*50/100))
+MINFIFTYPERCENT=$(($MYSTAKE - $FIFTYPERCENTSTAKE))
+MAXFIFTYPERCENT=$(($MYSTAKE + $FIFTYPERCENTSTAKE))
 BET=1
 
 #VARIABLES
 stake=$MYSTAKE
 
 #CHECK WIN OR LOOSE
-randomCheck=$((RANDOM%2))
-if (( randomCheck == 0 ))
-then
-	stake=$(($stake+$BET))
-else
-	stake=$(($stake-$BET))
-fi
+while (( stake >= MINFIFTYPERCENT && stake <= MAXFIFTYPERCENT ))
+do
+	randomCheck=$((RANDOM%2))
+	if (( randomCheck == 0 ))
+	then
+		stake=$(($stake+$BET))
+	else
+		stake=$(($stake-$BET))
+	fi
+done
