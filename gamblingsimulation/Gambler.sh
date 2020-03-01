40017ca7571892be20cf86bbfd9382ac01281e49
vampire16 <hakevaibhav16@gmail.com>
2020-03-01 20:14:18 +0530

    ADDED : Monthly days won and lost and by how much

--- gamblingsimulation/Gambler.sh
+++ gamblingsimulation/Gambler.sh
@@ -13,29 +13,46 @@ BET=1
 stake=$MYSTAKE
 win=0
 loose=0
+monthlystake=0
+day=0
+result=""
 
+read -p "Enter months : " MONTH
 #CHECK WIN OR LOOSE
-for (( index=1; index<=20; index++ ))
+for (( month=1; month<=MONTH; month++ ))
 do
-	stake=$MYSTAKE
-	while (( stake >= MINFIFTYPERCENT && stake <= MAXFIFTYPERCENT ))
+   for (( index=1; index<=20; index++ ))
 	do
-		randomCheck=$((RANDOM%2))
-		if (( randomCheck == 0 ))
+		stake=$MYSTAKE
+		while (( stake >= MINFIFTYPERCENT && stake <= MAXFIFTYPERCENT ))
+		do
+			randomCheck=$((RANDOM%2))
+			if (( randomCheck == 0 ))
+			then
+				stake=$(($stake+$BET))
+			else
+				stake=$(($stake-$BET))
+			fi
+		done
+		if (( stake > MAXFIFTYPERCENT ))
 		then
-			stake=$(($stake+$BET))
+			(( win++ ))
+			monthlystake=$(($monthlystake+$FIFTYPERCENTSTAKE))
+			result="win"
 		else
-			stake=$(($stake-$BET))
+			(( loose++ ))
+			monthlystake=$(($monthlystake-$FIFTYPERCENTSTAKE))
+			result="loose"
 		fi
+	((day++))
+	echo "$day day $FIFTYPERCENTSTAKE $result"
 	done
-	if (( stake > MAXFIFTYPERCENT ))
-	then
-		(( win++ ))
-	else
-		(( loose++ ))
-	fi
+	totalWin=$(($win*$FIFTYPERCENTSTAKE))
+	totalLoose=$(($loose*$FIFTYPERCENTSTAKE))
+	echo "Total win for a day : $totalWin"
+	echo "Total loose for a day : $totalLoose"
+	monthlystake=0
+	win=0
+	loose=0
+	day=0
 done
-totalWin=$(($win*$FIFTYPERCENTSTAKE))
-totalLoose=$(($loose*$FIFTYPERCENTSTAKE))
-echo "Total win for a day : $totalWin"
-echo "Total loose for a day : $totalLoose"
